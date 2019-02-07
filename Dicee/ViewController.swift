110c73c705277e01f0a66667a600afb41c349301
Your Name <you@example.com>
2019-02-07 13:08:25 -0700

    new code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     var randomDiceIndex1 : Int = 0
-    
     var randomDiceIndex2 : Int = 0
     
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var randomDiceImage1: UIImageView!
+    @IBOutlet weak var randomDiceImage2: UIImageView!
+    
+   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +26,10 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-      randomDiceIndex1 = Int(arc4random_uniform (6))
-        
-      randomDiceIndex2 = Int(arc4random_uniform (6))
-        
-        print(randomDiceIndex1)
-    
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
     }
+    
     
 }
 
