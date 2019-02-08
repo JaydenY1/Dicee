75d95f9c92926a5905d65f7748fbc6eee8a13ef7
Your Name <you@example.com>
2019-02-08 11:55:03 -0700

    added motion controls

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
-    @IBOutlet weak var randomDiceImage1: UIImageView!
-    @IBOutlet weak var randomDiceImage2: UIImageView!
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
    
     
@@ -25,13 +27,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+     
+        updateDiceImages()
         
+    }
+        func updateDiceImages(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
+    
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+       
+        updateDiceImages()
+        
+    }
     
 }
 
